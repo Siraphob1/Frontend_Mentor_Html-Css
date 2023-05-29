5ef9012533c93a979ab609da72d1d45da768e16c
Siraphob1 <Sirapop_t@hotmail.com>
2023-05-29 22:17:15 +0700

    Challange8 add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Challange8_blogr-landing-page-main/src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
